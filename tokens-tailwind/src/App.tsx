dafd336b76d19074ef603b9ca38b384a16bfe1d8
Gilles Deketelaere <gdeketelaere@gmail.com>
2023-05-09 14:18:29 +0200

    🎨 Add tailwind config

---
+++ tokens-tailwind/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./style/App.css";
+import { Button } from "./components/buttons/button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
